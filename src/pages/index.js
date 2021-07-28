5bf784d8bb386f87747665da4271f104b5c5d5d1
Kegan Bako-Listy <bakolisty@gmail.com>
2021-07-28 09:25:25 -0700

    Adding another project to the projects section, and reordering the list of projects.

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,13 +5,21 @@ import ProjectBlock from "../components/project-block"
 export default () => (
     <Layout>
         <h1>Welcome!</h1>
-        <p>My name is Kegan, and this is my website. Feel free to check out my projects on Github, or my profile on LinkedIn.</p>
+        <p>My name is Kegan, and this is my website. I am a Software Engineer who enjoys working on fun, and silly interactive projects. Feel free to check out my projects on Github, or my profile on LinkedIn.</p>
         <h1>Projects: </h1>
         <ul>
             <li>
                 <ProjectBlock
-                    projectName="Is It Coffee"
-                    projectDescription="- A Python web application written in Flask. It uses the Google Cloud Vision API to detect coffee in uploaded images."
+                    projectName="Untitled Marble Game"
+                    projectDescription="- A project I am currently working on to teach myself Unity, and video game development concepts. 
+                                        The general idea I have for this game is a multiplayer battle royale where the players are all marbles. 
+                                        You will be able to eliminate other players by knocking them out of the arena."
+                />
+            </li>
+            <li>
+                <ProjectBlock
+                    projectName="Cilantro Audit"
+                    projectDescription="- A desktop application used to create, fill out, and view the results of forms. This project was written in Python, and uses MongoDB."
                 />
             </li>
             <li>
@@ -22,8 +30,8 @@ export default () => (
             </li>
             <li>
                 <ProjectBlock
-                    projectName="Cilantro Audit"
-                    projectDescription="- A desktop application used to create, fill out, and view the results of forms. This project was written in Python, and uses MongoDB."
+                    projectName="Is It Coffee"
+                    projectDescription="- A Python web application written in Flask. It uses the Google Cloud Vision API to detect coffee in uploaded images."
                 />
             </li>
         </ul>
